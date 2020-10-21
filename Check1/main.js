cc988572fe7a3e1828d00b8339c92c0a9f3612b7
RyoTsuyuzaki <69714943+RyoTsuyuzaki@users.noreply.github.com>
2020-10-21 17:08:35 +0900

    条件分岐・演算子　課題提出
    問１ 理論演算子を使用しての表示
    問2.問3 条件分岐記述

--- Check1/main.js
+++ Check1/main.js
@@ -1,2 +1,37 @@
+let numbers = [2, 5, 12, 13, 15, 18, 22];
+//ここに答えを実装してください。↓↓↓
+let index = 0
+function isEven(num) {
+    while (index < num.length) {
+        if(num[index] % 2 == 0) {
+            console.log(num[index] + 'は偶数です');
+        }
+        index++;
+    };
+};
+isEven(numbers);
+// -------------------------------------------------
+
 // let numbers = [2, 5, 12, 13, 15, 18, 22];
+// //ここに答えを実装してください。↓↓↓
+// const filternum = numbers.filter(function isEven(num) {
 //     return num % 2 == 0 
+// });
+// console.log(filternum + 'は偶数です');
+
+// --------------------------------------------------
+
+
+class Car {
+    constructor(oil, number){
+        this.oil = oil;
+        this.number = number;
+    }
+
+    getNumGas() {
+        console.log(`ガソリンは${this.oil}です。ナンバーは${this.number}です`);
+    }
+
+}
+let cars = new Car(`○○`, `△△`);
+cars.getNumGas();
